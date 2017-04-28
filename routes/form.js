2b063f4433ead9dd44881db5cd8898dce35bfb7d
naeohmi <naeohmi@gmail.com>
2017-04-28 15:26:02 -0400

    setup

---
+++ routes/form.js
@@ -0,0 +1,62 @@
+let payLoad = document.querySelector('#payload');
+
+//stubs
+function createItem(e) {
+    e.preventDefault();
+
+    console.log('create a resource')
+}
+
+
+function readAllItems(e) {
+    e.preventDefault();
+    console.log('read all the resources');
+    axios.get('https://intense-headland-84262.herokuapp.com/api/tasks/')
+        .then(function(res) {
+            console.log(res.data.data);
+            payLoad.innerHTML = "";
+            res.data.data.forEach(function(d, c) {
+                payLoad.innerHTML += `<div>${c + 1}. ${d.item} 
+        <span id=itemid_${d.id}>❌</span>
+        </div>`
+            })
+        })
+        .catch(function(err) {
+            console.log(err)
+        })
+}
+
+function readItem(e) {
+    e.preventDefault();
+    console.log('read one item');
+}
+
+function updateItem(e) {
+    e.preventDefault();
+    console.log('update a resource');
+}
+
+function deleteItem(e) {
+    e.preventDefault();
+    console.log('delete a resource');
+
+    if (event.target.tagName.toLowerCase() === 'span') {
+        let e_id = event.target.id;
+        confirm(`Are you sure you want to delete item with the ID ${e_id.split('_')[1]} 
+    \n I mean really!
+    \n Also, consider using something better than this fugly looking confirm window
+    \n Use a library like bootstrap, or some modal library...`);
+    }
+}
+
+//register event listeners
+document.getElementById('create_btn').addEventListener('click', createItem);
+document.getElementById('readall_btn').addEventListener('click', readAllItems);
+document.getElementById('read_btn').addEventListener('click', readItem);
+document.getElementById('update_btn').addEventListener('click', updateItem);
+document.getElementById('delete_btn').addEventListener('click', deleteItem);
+
+document.querySelectorAll('#payload') //event delegation, delegate event to the parent 
+    .forEach((c) => {
+        c.addEventListener('click', deleteItem)
+    });
